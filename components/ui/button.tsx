0773449704c139c411ba176bfbb78e4dba54b86e
brijesh <meetbrij@gmail.com>
2025-11-02 09:17:55 +0530

    update styling for primary buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-gray-700 text-white shadow hover:bg-gray-600",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
